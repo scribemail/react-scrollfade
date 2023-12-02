d152a81c15fbb1f6c1152c5a373c1f7f44e228e1
korhox <juuso.korhonen@bittivirta.fi>
2023-12-02 20:00:41 +0200

    feat: :sparkles: Add support for intensity of the gradient

--- lib/ScrollFade.tsx
+++ lib/ScrollFade.tsx
@@ -1,25 +1,29 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 
 const easeIn = (t: number, alpha: number): number => { return Math.pow(t, alpha) };
-const FADE_HEIGHT = 56;
 
-interface ScrollFadeProps { height?: number };
+interface ScrollFadeProps { height?: number, intensity?: number };
 
-const ScrollFade: React.FC<ScrollFadeProps> = ({ height }) => {
+const ScrollFade: React.FC<ScrollFadeProps> = ({ height = 64, intensity = 0 }) => {
     const rootRef = useRef<HTMLDivElement>(null);
     const getMaskRef = useRef<(opacity: number) => string>(
-        (opacity: number) => `linear-gradient(180deg, black, rgba(0,50, 255, ${opacity})) center bottom/100%
-        ${height}px no-repeat,
-    linear-gradient(180deg, black, black) center top/100% calc(100% - ${height}px)
-        no-repeat`
+        (opacity: number) => {
+            return (`
+                linear-gradient(180deg, black 0%, rgba(255, 255, 255, ${opacity}) ${(100 - intensity) - 1}%)
+                    center bottom/100% ${height}px no-repeat,
+                linear-gradient(180deg, black, black)
+                    center top/100% calc(100% - ${height}px) no-repeat
+            `)
+        }
     );
+
 
     const onScroll = useCallback(() => {
         const scrollElement = rootRef.current?.parentElement;
         if (scrollElement !== null && scrollElement !== undefined) {
             const getMask = getMaskRef.current;
             const { offsetHeight: elementHeight, scrollHeight: elementWidth, scrollTop } = scrollElement;
-            const opacity = easeIn(scrollTop / (elementHeight - elementWidth), 10);
+            let opacity = easeIn(scrollTop / (elementHeight - elementWidth), 10);
             const mask = getMask(opacity);
 
             scrollElement.style.mask = mask;
@@ -44,8 +48,5 @@ const ScrollFade: React.FC<ScrollFadeProps> = ({ height }) => {
     }, []);
 
     return <div className="scroll-fade" ref={rootRef} />;
-};
-ScrollFade.defaultProps = {
-    height: FADE_HEIGHT,
 };
 export default ScrollFade;
