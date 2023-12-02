07163ebf39618a49d5cfffff9d6c9e2f4aef4c03
korhox <juuso.korhonen@bittivirta.fi>
2023-12-02 19:04:48 +0200

    fix: :bug: Update example to React 18

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
